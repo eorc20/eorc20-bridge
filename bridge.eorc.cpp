72b3dae268e258b4f4fad37092ceb4620066e29d
Denis Carriere <carriere.denis@gmail.com>
2023-12-31 17:15:33 -0500

    add IERC7583

--- bridge.eorc.cpp
+++ bridge.eorc.cpp
@@ -17,19 +17,21 @@ void bridge::on_nft_transfer( const name from, const name to, const vector<uint6
 void bridge::onbridgemsg(const bridge_message_t message)
 {
     const bridge_message_v0 &msg = std::get<bridge_message_v0>(message);
+    const bytes data = {msg.data.begin(), msg.data.end()};
 
     check(msg.receiver == get_self(), "invalid message receiver");
+    const checksum256 from = make_key(bytes{data.begin(), data.begin() + 20});
     const checksum256 sender = make_key(msg.sender);
     const checksum256 value = make_key(msg.value);
     const checksum256 trx_id = get_trx_id();
-    const checksum256 from = make_key("0x91dFF55CD58c70175BC9A9230C4c2c2A57e9F61E");
-    const std::string data(msg.data.begin(), msg.data.end());
+    const string calldata = {data.begin() + 20, data.end()};
+    // const std::string data(msg.data.begin(), msg.data.end());
 
     print("from: ", from,
         "\nreceiver: ", msg.receiver,
         "\nsender: ", sender,
         "\ntrx_id: ", trx_id,
-        "\ndata: ", data,
+        "\ncalldata: ", calldata,
         "\nvalue: ", value
     );
 }
--- bridge.eorc.sol
+++ bridge.eorc.sol
@@ -1,25 +1,38 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.4.16 <0.9.0;
+pragma solidity 0.8.x;
 
+interface IERC7583 {
+    event Inscribe(bytes data);
+}
+
+// https://ethereum-magicians.org/t/discussion-on-erc-7583-for-inscribing-data-in-smart-contract/17661
 // 0x646174613a2c7b2270223a22656f72633230222c226f70223a227472616e73666572222c227469636b223a22656f7373222c22616d74223a223130227d
 // data:,{"p":"eorc20","op":"transfer","tick":"eoss","amt":"10"}
-contract BridgeEORC20 {
+contract BridgeEORC20 is IERC7583 {
     address public evmAddress = 0xbBBBbBbbbBBBBbbbbbbBBbBB5530EA015b900000;
     string  public account = "bridge.eorc";
+    address public owner = 0xbBbbBBbBbbBBbbbbbbbbBbBB3Ddc96280Aa5D000;
     event Received(address, uint256);
-    event Data(address, bytes);
+    event CallData(address, bytes);
 
-    function _isReservedAddress(address addr) internal pure returns (bool) {
-        return ((uint160(addr) & uint160(0xFffFfFffffFfFFffffFFFffF0000000000000000)) == uint160(0xBBbbBbBbbBbbBbbbBbbbBBbb0000000000000000));
+    function inscribe(bytes calldata data) public {
+        require(msg.sender == owner);
+        emit Inscribe(data);
     }
 
+    // This function is executed when a contract receives plain Ether (without data)
     receive() external payable {
-        emit Received(msg.sender, msg.value);
+        revert();
     }
 
     fallback() external payable {
-        emit Data(msg.sender, msg.data);
-        (bool success, ) = evmAddress.call{value: msg.value}(abi.encodeWithSignature("bridgeMsgV0(string,bool,bytes)", account, true, msg.data ));
+        // add `from` to calldata
+        bytes memory data = abi.encodePacked(msg.sender, msg.data);
+
+        // push calldata to EOS Native
+        (bool success, ) = evmAddress.call{value: msg.value}(
+            abi.encodeWithSignature("bridgeMsgV0(string,bool,bytes)", account, true, data )
+        );
         if (!success) { revert(); }
     }
 }
