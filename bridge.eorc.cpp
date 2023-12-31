02c9802bb4b634d367f49b9e9a99b111dd9fcc0c
Denis Carriere <carriere.denis@gmail.com>
2023-12-30 23:14:16 -0500

    add onbridgemsg

--- bridge.eorc.cpp
+++ bridge.eorc.cpp
@@ -10,3 +10,24 @@ void bridge::on_nft_transfer( const name from, const name to, const vector<uint6
 
     check(false, "not implemented");
 }
+
+[[eosio::action]]
+void bridge::onbridgemsg(const bridge_message_t message)
+{
+    const bridge_message_v0 &msg = std::get<bridge_message_v0>(message);
+
+    check(msg.receiver == get_self(), "invalid message receiver");
+    checksum256 addr_key = make_key(msg.sender);
+    // print(msg.receiver, addr_key);
+}
+
+checksum256 bridge::make_key(bytes data) {
+    return make_key((const uint8_t *)data.data(), data.size());
+}
+
+checksum256 bridge::make_key(const uint8_t *ptr, size_t len) {
+    uint8_t buffer[32] = {};
+    check(len <= sizeof(buffer), "len provided to make_key is too small");
+    memcpy(buffer, ptr, len);
+    return checksum256(buffer);
+}
--- bridge.eorc.hpp
+++ bridge.eorc.hpp
@@ -6,13 +6,29 @@
 using namespace eosio;
 using namespace std;
 
+typedef std::vector<uint8_t> bytes;
+
 class [[eosio::contract("bridge.eorc")]] bridge : public eosio::contract {
+
 public:
     using contract::contract;
 
+
+    struct bridge_message_v0 {
+        eosio::name receiver;
+        bytes sender;
+        eosio::time_point timestamp;
+        bytes value;
+        bytes data;
+    };
+    using bridge_message_t = std::variant<bridge_message_v0>;
+
+    [[eosio::action]]
+    void onbridgemsg(const bridge_message_t message);
+
     [[eosio::on_notify("atomicassets::transfer")]]
     void on_nft_transfer( const name from, const name to, const vector<uint64_t> asset_ids, const std::string memo );
-
 private:
-
+    checksum256 make_key(bytes data);
+    checksum256 make_key(const uint8_t *ptr, size_t len);
 };
