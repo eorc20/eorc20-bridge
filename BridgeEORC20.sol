8b7e788f7da662e4d10062731718bfdd6acae281
Denis Carriere <carriere.denis@gmail.com>
2024-01-14 22:14:10 -0500

    add deploy operation

--- BridgeEORC20.sol
+++ BridgeEORC20.sol
@@ -7,20 +7,31 @@ import {Strings} from "./contracts/utils/Strings.sol";
 
 // https://ethereum-magicians.org/t/discussion-on-erc-7583-for-inscribing-data-in-smart-contract/17661
 interface IERC7583 {
-    event TransferIns(address indexed from, address indexed to, uint256 indexed id);
-    event Inscribe(uint256 indexed id, bytes data);
+    event TransferIns(address indexed from, address indexed to, uint64 indexed id);
+    event Inscribe(uint64 indexed id, bytes data);
 }
 
 contract BridgeEORC20 is ERC20, Ownable, IERC7583 {
     address public evmAddress = 0xbBBBbBbbbBBBBbbbbbbBBbBB5530EA015b900000; // reserved address for eosio.evm
     address public bridgeAddress = 0xbBbbBBbBbbBBbbbbbbbbBbBB3Ddc96280Aa5D000; // reserved address for bridge.eorc
-    string  public bridgeAccount = "bridge.eorc";
-    uint256 public id = 0;
+    string public bridgeAccount = "bridge.eorc";
+    uint64 public id = 0; // Inscription ID
+    string public p = "eorc20"; // Protocol
+    int64 public max; // The maximum supply of the token
+    int64 public lim; // The mint limit per inscription
+    string public tick; // The token ticker
 
     constructor(
         string memory _name,
-        string memory _symbol
-    ) ERC20(_name, _symbol) Ownable(bridgeAddress) {}
+        string memory _tick,
+        int64 _max,
+        int64 _lim
+    ) ERC20(_name, _tick) Ownable(bridgeAddress) {
+        max = _max;
+        lim = _lim;
+        tick = _tick;
+        _inscribe(_msgSender(), address(this), deployOp());
+    }
 
     function decimals() public view virtual override returns (uint8) {
         return 0;
@@ -35,41 +46,57 @@ contract BridgeEORC20 is ERC20, Ownable, IERC7583 {
     }
 
     function _update(address from, address to, uint256 value) override internal {
+        _beforeTokenTransfer(from, to, value);
         super._update(from, to, value);
         _afterTokenTransfer(from, to, value);
-        _inscribe(from, to, value);
+        _inscribe(from, to, transferOp(value));
     }
 
     function _isReservedAddress(address addr) internal pure returns (bool) {
         return ((uint160(addr) & uint160(0xFffFfFffffFfFFffffFFFffF0000000000000000)) == uint160(0xBBbbBbBbbBbbBbbbBbbbBBbb0000000000000000));
     }
 
+    function _beforeTokenTransfer(address from, address to, uint256 value) internal {
+        // ignore mint and burn
+        if (from == address(0) || to == address(0)) return;
+
+        if (from == bridgeAddress) {
+            require(_msgSender() == bridgeAddress, "unauthorized bridge sender");
+            _mint(from, value);
+        }
+    }
+
     function _afterTokenTransfer(address from, address to, uint256 value) internal {
         // ignore mint and burn
         if (from == address(0) || to == address(0)) return;
+
         if (_isReservedAddress(to)) {
-            _notifyBridge(from, to, value);
             _burn(to, value);
         }
     }
 
-    function _notifyBridge(address from, address to, uint256 value) internal {
-        bytes memory data = transferOp(symbol(), value);
-        bytes memory receiver_msg = abi.encodePacked(from, to, data);
-        (bool success, ) = evmAddress.call{value: msg.value}(abi.encodeWithSignature("bridgeMsgV0(string,bool,bytes)", bridgeAccount, true, receiver_msg ));
-        if (!success) { revert("error sending bridge message"); }
-    }
-
-    // Inscription events on Transfer
-    function _inscribe(address from, address to, uint256 value) internal {
-        bytes memory data = transferOp(symbol(), value);
+    // Inscription events on `deploy`, `mint` & `transfer`
+    function _inscribe(address from, address to, bytes memory data) internal {
         emit Inscribe( id, data );
         emit TransferIns( from, to, id );
+        _notifyBridge(from, to, data);
         id++;
     }
 
-    function transferOp(string memory tick, uint256 value) internal pure returns (bytes memory) {
-        return abi.encodePacked('data:,{"p":"eorc20","op":"transfer","tick":"', tick, '","amt":"', Strings.toString(value), '"}');
+    function _notifyBridge(address from, address to, bytes memory data) internal {
+        bytes memory receiver_msg = abi.encodePacked(id, from, to, data);
+        (bool success, ) = evmAddress.call{value: msg.value}(
+            abi.encodeWithSignature("bridgeMsgV0(string,bool,bytes)", bridgeAccount, true, receiver_msg )
+        );
+        if (!success) { revert("error with bridge message"); }
+    }
+
+    function transferOp(uint256 value) internal view returns (bytes memory) {
+        return abi.encodePacked('data:,{"p":"', p,'","op":"transfer","tick":"', tick, '","amt":"', Strings.toString(value), '"}');
+    }
+
+    function deployOp() internal view returns (bytes memory) {
+        return abi.encodePacked('data:,{"p":"', p,'","op":"deploy","tick":"', tick, '","max":"', max, '","lim":"', lim, '"}');
     }
 
     // This function is executed when a contract receives plain Ether (without data)
@@ -77,20 +104,22 @@ contract BridgeEORC20 is ERC20, Ownable, IERC7583 {
         revert("cannot call contract without calldata");
     }
 
-    // Handles incoming inscriptions using calldata
-    //
-    // data:,{"p":"eorc20","op":"transfer","tick":"eoss","amt":"10"}
-    // 0x646174613a2c7b2270223a22656f72633230222c226f70223a227472616e73666572222c227469636b223a22656f7373222c22616d74223a223130227d
+    /**
+     * Handles incoming inscriptions using calldata
+     *
+     * transfer
+     * data:,{"p":"eorc20","op":"transfer","tick":"eoss","amt":"10"}
+     * 0x646174613a2c7b2270223a22656f72633230222c226f70223a227472616e73666572222c227469636b223a22656f7373222c22616d74223a223130227d
+     *
+     * mint
+     * data:,{"p":"eorc20","op":"mint","tick":"eoss","amt":"10000"}
+     * 0x646174613a2c7b2270223a22656f72633230222c226f70223a226d696e74222c227469636b223a22656f7373222c22616d74223a223130303030227d
+     */
     fallback() external payable {
         address from = _msgSender();
-        address to = address(this);
         require(tx.origin == from, "contracts not allowed");
-
-        // append `from` sender with message calldata
-        bytes memory receiver_msg = abi.encodePacked(from, to, _msgData());
-
-        // push calldata to EOS Native
-        (bool success, ) = evmAddress.call{value: msg.value}(abi.encodeWithSignature("bridgeMsgV0(string,bool,bytes)", bridgeAccount, true, receiver_msg ));
-        if (!success) { revert(); }
+        address to = address(this);
+        bytes memory data = _msgData();
+        _inscribe(from, to, data);
     }
 }
